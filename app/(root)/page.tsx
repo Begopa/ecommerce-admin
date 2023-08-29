32b43a5149135808997ddf09309a2f8ef066a15d
Begopa <philip0813@naver.com>
2023-08-29 16:00:11 +0900

    feat(store modal): 기본 첫 페이지에서 modal이 열리도록 trigger 추가, 기본 페이지에서는 store을 생성하기 전까지 modal이 종료되지 않음

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,20 +1,21 @@
 "use client";
 import { UserButton } from "@clerk/nextjs";
 import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 const SetupPage = () => {
-  return (
-    <div className="p-4">
-      <Modal
-        title="Test"
-        description="Test Desc"
-        isOpen={true}
-        onClose={() => {}}
-      >
-        Children
-      </Modal>
-    </div>
-  );
+  // const storeModal = useStoreModal(); <-- useEffect 안에서 못 씀
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div className="p-4">Root Page</div>;
 };
 
 export default SetupPage;
