4439de2e51e6fc6160823c89a1903602414289e7
Begopa <philip0813@naver.com>
2023-09-07 17:07:13 +0900

    feat(all-form): 제출 폼에 반응형 추가

--- app/(dashboard)/[storeId]/(routes)/colors/[colorId]/components/color-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/[colorId]/components/color-form.tsx
@@ -124,7 +124,7 @@ export const ColorForm: React.FC<ColorFormProps> = ({ initialData }) => {
           onSubmit={form.handleSubmit(onSubmit)}
           className="space-y-8 w-full"
         >
-          <div className="grid grid-cols-3 gap-8">
+          <div className="md:grid md:grid-cols-3 gap-8">
             <FormField
               control={form.control}
               name="name"
--- app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/components/size-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/components/size-form.tsx
@@ -122,7 +122,7 @@ export const SizeForm: React.FC<SizeFormProps> = ({ initialData }) => {
           onSubmit={form.handleSubmit(onSubmit)}
           className="space-y-8 w-full"
         >
-          <div className="grid grid-cols-3 gap-8">
+          <div className="md:grid md:grid-cols-3 gap-8">
             <FormField
               control={form.control}
               name="name"
