354ce781af005cbccd447984e970836bce1c8c48
Begopa <philip0813@naver.com>
2023-09-07 17:00:05 +0900

    feat(product): products 데이터 테이블에서 가격 앞에 통화 기호를 표시하기 위한 formatter 추가

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,16 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatterForUSD = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+
+export const formatterForKRW = new Intl.NumberFormat("ko-KR", {
+  style: "currency",
+  currency: "KRW",
+});
