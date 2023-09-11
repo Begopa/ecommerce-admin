8aae48d239f6893652e314d549cc8fcd7e136285
Begopa <philip0813@naver.com>
2023-09-11 16:30:59 +0900

    fix(overview): overview 오타 수정 및 import 정리

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,29 +1,30 @@
-import { CreditCard, DollarSign } from "lucide-react";
+import { CreditCard, DollarSign, Package } from "lucide-react";
 
+import { Separator } from "@/components/ui/separator";
+import { Overview } from "@/components/overview";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Heading } from "@/components/ui/heading";
-import { Separator } from "@/components/ui/separator";
-import prismadb from "@/lib/prismadb";
-import { formatterForUSD } from "@/lib/utils";
 import { getTotalRevenue } from "@/actions/get-total-revenue";
 import { getSalesCount } from "@/actions/get-sales-count";
-import { getStockCount } from "@/actions/get-stock-count";
-import { Overview } from "@/components/overview";
 import { getGraphRevenue } from "@/actions/get-graph-revenue";
+import { getStockCount } from "@/actions/get-stock-count";
+import { formatterForUSD } from "@/lib/utils";
 
 interface DashboardPageProps {
-  params: { storeId: string };
+  params: {
+    storeId: string;
+  };
 }
 
 const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
   const totalRevenue = await getTotalRevenue(params.storeId);
   const salesCount = await getSalesCount(params.storeId);
-  const stockContent = await getStockCount(params.storeId);
+  const stockCount = await getStockCount(params.storeId);
   const graphRevenue = await getGraphRevenue(params.storeId);
   return (
     <div className="flex-col">
-      <div className="flex-1 space-y-4 p-8 py-6">
-        <Heading title="Dashboard" description="overview of your store" />
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <Heading title="Dashboard" description="Overview of your store" />
         <Separator />
         <div className="grid gap-4 grid-cols-3">
           <Card>
@@ -53,22 +54,22 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
               <CardTitle className="text-sm font-medium">
                 Products In Stock
               </CardTitle>
-              <DollarSign className="h-4 w-4 text-muted-foreground" />
+              <Package className="h-4 w-4 text-muted-foreground" />
             </CardHeader>
             <CardContent>
-              <div className="text-2xl font-bold">{stockContent}</div>
+              <div className="text-2xl font-bold">{stockCount}</div>
             </CardContent>
           </Card>
         </div>
+        <Card className="col-span-4">
+          <CardHeader>
+            <CardTitle>Overview</CardTitle>
+          </CardHeader>
+          <CardContent className="pl-2">
+            <Overview data={graphRevenue} />
+          </CardContent>
+        </Card>
       </div>
-      <Card className="col-span-4">
-        <CardHeader>
-          <CardTitle>OverView</CardTitle>
-        </CardHeader>
-        <CardContent className="pl-2">
-          <Overview data={graphRevenue} />
-        </CardContent>
-      </Card>
     </div>
   );
 };
